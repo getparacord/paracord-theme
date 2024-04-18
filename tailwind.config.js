36ce914f76ba73792fd1e12353895dddb767a5c2
Justin Mitchel <jmitchel3@gmail.com>
2024-04-18 11:58:24 -0600

    Added deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "../paracord/**/*.{html,js}"
+    /* Local paracord Django project templates */
+    "../paracord/**/*.{html,js}",
+    /* Theme html files */
+    'src/**/*.html',
+     /**
+     * Flowbite: If you do not use Flowbite components, comment out the following line
+     */
+    './node_modules/flowbite/**/*.js'
   ],
   theme: {
     extend: {},
